ebb14c193b3c692a72783dab289ba71193b5941c
Mengjun Liu <mengjun_liu@126.com>
2019-05-28 20:12:26 +0800

    Fix: add u in INLINE_TAGS

--- pyquery/text.py
+++ pyquery/text.py
@@ -23,7 +23,7 @@ INLINE_TAGS = {
     'a', 'abbr', 'acronym', 'b', 'bdo', 'big', 'br', 'button', 'cite',
     'code', 'dfn', 'em', 'i', 'img', 'input', 'kbd', 'label', 'map',
     'object', 'q', 'samp', 'script', 'select', 'small', 'span', 'strong',
-    'sub', 'sup', 'textarea', 'time', 'tt', 'var'
+    'sub', 'sup', 'textarea', 'time', 'tt', 'u', 'var'
 }
 
 SEPARATORS = {'br'}
